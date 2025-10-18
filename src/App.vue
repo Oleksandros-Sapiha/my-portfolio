7c0ee182d3e0d73dc08e6fe9543d82b041451c88
Oleksandros-Sapiha <mobaksterritory@gmail.com>
2025-10-17 20:53:44 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Homepage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Homepage from './pages/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Homepage
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: system-ui, Roboto, sans-serif;
+  }
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,110 @@
+<template>
+   <div>
+      <img src="BackgroundImage.jpg" class="background-image">
+   </div>
+   <div class="homepage-card"> <br>
+      <h2>Mikoro</h2>
+      <p>✦ me when I obsess over fictional worldbuilding stuff cuz I can ✦</p>
+      <div class="links"> 
+        <a href="#">POSTS</a>
+        <a href="#">ASK ME ANYTHING</a>
+        <a href="#">ARCHIVE</a>
+      </div>
+      <p>Just some Worldbuilder Fella who’s obsessed with Rainworld and Military stuff. 
+      Made that website with the help of my Online Web-designing Class Teacher, Dmytro, he’s a cool guy, thanks to him for everything.
+      one of the members of PROJECT: STARFALL</p>
+      <ul>
+         <li>Beginner in Digital Art</li>
+         <li>Worldbuilder</li>
+         <li>Traditional Artist</li>
+         <li>Roblox Studio Vignette creator</li>
+      </ul> <br>
+   </div>
+   <div class="post-content">
+      <img src="PostImage.png" class="image">
+      <p>Some cool tree models available for y’all, don’t miss</p>
+      <button>DOWNLOAD</button>
+   </div>
+</template>
+
+<script scoped>
+
+</script>
+
+<style>
+   body {
+      background-color: #171A21;
+   }
+   .links {
+      text-decoration: none;
+      color: #E20346;
+      display: flex;
+      justify-content: space-evenly;
+      list-style: none;
+      gap: 3px;
+      font-weight: bold;
+      margin-left: 40px;
+   }
+   .homepage-card {
+      background-color: #1B2126;
+      border-radius: 25px;
+      margin-top: 440px;
+      width: 1000px;
+      padding: 103px, 683px;
+   }
+   .homepage-card, h2 {
+      text-align: center;
+      color: #E20346;
+      font-weight: bold;
+      font-size: 45px;
+      margin-left: 20px;
+   }
+   .homepage-card, p {
+      font-weight:normal;
+      text-align: center;
+      color: #A20435;
+      font-size: 13px;
+   }
+   ul {
+      font-weight:normal;
+      text-align: center;
+      color:#A20435;
+      font-size: 13px;
+      list-style: none;
+   }
+   .post-content {
+      max-width: 315px;
+      border: none;
+      background-color: white;
+      border-radius: 8px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      margin-top: 30px;
+   }
+   .post-content p{
+      color: #555555;
+      margin-left: 5px;
+      text-align: left;
+   }
+   button {
+      background: linear-gradient(to bottom, #328B12, #2E6619);
+      height: 68px;
+      width: 141px;
+      color: white;
+      font-weight: bold;
+      text-align: left;
+      border: none;
+      border-radius: 3px;
+      font-size: 19px;
+      cursor: pointer;
+      margin-bottom: 13px;
+      margin-left: 5px;
+   }
+   image {
+      width: 315px;
+      height: 325px;
+      object-fit: cover;
+   }
+   .background-image {
+      padding: 1440px, 601px;
+   }
+</style>
